9a1908f150b81b1d3e8c93dab29d00958c8154e3
Agung Setiawan <agung.joko@iconpln.co.id>
2025-06-21 11:39:38 +0700

    initialization

---
+++ src/main/java/com/icon/be_web_ecommerce/BeWebEcommerceApplication.java
@@ -0,0 +1,13 @@
+package com.icon.be_web_ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeWebEcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeWebEcommerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/icon/be_web_ecommerce/BeWebEcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.icon.be_web_ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeWebEcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
